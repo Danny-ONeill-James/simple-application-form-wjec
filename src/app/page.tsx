d3fc1ab55e429bb0cfb1ca094f88afde4b6e3f99
Danny <116497508+Danny-ONeill-James@users.noreply.github.com>
2023-08-17 12:52:22 +0100

    feat: set up app items

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <h1 className="text-3xl font-bold underline">Hello World!</h1>
+      <h2 className="text-2xl underline">Sub</h2>
+    </main>
+  );
 }
