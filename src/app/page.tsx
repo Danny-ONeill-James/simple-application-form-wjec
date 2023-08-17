2da466f02550385e7f3bea5c5ee36935aba74161
Danny <116497508+Danny-ONeill-James@users.noreply.github.com>
2023-08-17 19:50:38 +0100

    refactor: cleaned up and moved form for now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,46 @@
-import Form from "./components/form";
+import FormFieldsGroup from "./components/formFieldsGroup";
+import FormSection from "./components/formSection";
+import FormSideBar from "./components/formSideBar";
+import FormTextField from "./components/formTextField";
 
 export default function Home() {
   return (
-    <main className="">
-      <Form />
+    <main className="min-h-screen p-6 bg-gray-100 flex items-center justify-center">
+      <div className="container max-w-screen-lg mx-auto">
+        <form>
+          <h2 className="font-semibold text-xl text-gray-600">
+            Application Form
+          </h2>
+          <p className="text-gray-500 mb-6">
+            This is an example form for WJEC.
+          </p>
+          <FormSection>
+            <FormSideBar
+              title="Role"
+              paragraph="Details on the position you are applying for."
+            />
+            <FormFieldsGroup>
+              <FormTextField
+                IdAndName={"Position"}
+                label={"Details of the position you are applying for."}
+                required={true}
+              />
+              <FormTextField
+                IdAndName={"Position"}
+                label={"Details of the position you are applying for."}
+                required={true}
+              />
+            </FormFieldsGroup>
+          </FormSection>
+          <div className="md:col-span-5 text-right">
+            <div className="inline-flex items-end">
+              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                Submit
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
     </main>
   );
 }
