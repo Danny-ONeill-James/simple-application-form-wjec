391e0de5f2377da803f811ede13a1bfa374020c5
Danny O'Neill-James <116497508+Danny-ONeill-James@users.noreply.github.com>
2023-08-17 23:03:43 +0100

    Feat  introduce localisation (#2)
    * refactor: cleaned up and moved form for now
    
    * refactor: moved components to correct location:
    
    * reactor: Used Docs, replaced components
    
    * feat: base localisation now working
    
    * chore: Initial localisation Data added
    
    * feat: added on change to text input
    
    * refactor: removed unused state call
    
    * feat: set toggle field ready
    
    * refactor: removed unuised State
    
    * feat: added Personal Details Form Fields

---
+++ src/app/[locale]/page.tsx
@@ -0,0 +1,193 @@
+"use client";
+
+import FormFieldsGroup from "@/components/formFieldsGroup";
+import FormSection from "@/components/formSection";
+import FormSideBar from "@/components/formSideBar";
+import FormTextField from "@/components/formTextField";
+import FormToggleField from "@/components/formToggleField";
+import { useTranslations } from "next-intl";
+import { useState } from "react";
+
+export default function Home() {
+  const t = useTranslations("IndexPage");
+
+  return (
+    <main className="p-6 bg-gray-100  items-center justify-center">
+      <div className="container max-w-screen-lg mx-auto">
+        <form>
+          <h2 className="font-semibold text-xl text-gray-600">
+            {t("form.title")}
+          </h2>
+          <p className="text-gray-500 mb-6">{t("form.description")}</p>
+          <FormSection>
+            <FormSideBar
+              title={t("form.section.role.title")}
+              paragraph={t("form.section.role.description")}
+            />
+            <FormFieldsGroup>
+              <FormTextField
+                IdAndName="position"
+                label={t("form.section.role.questions.position")}
+                required={true}
+              />
+            </FormFieldsGroup>
+          </FormSection>
+          <FormSection>
+            <FormSideBar
+              title={t("form.section.eligibility.title")}
+              paragraph={t("form.section.eligibility.description")}
+            />
+            <FormFieldsGroup>
+              <FormToggleField
+                IdAndName="entitled"
+                label={t("form.section.eligibility.questions.entitled")}
+              />
+              <FormTextField
+                IdAndName="detail"
+                label={t("form.section.eligibility.questions.detail")}
+                required={true}
+              />
+            </FormFieldsGroup>
+          </FormSection>
+          <FormSection>
+            <FormSideBar
+              title={t("form.section.personalDetails.title")}
+              paragraph={t("form.section.personalDetails.description")}
+            />
+            <FormFieldsGroup>
+              <FormTextField
+                IdAndName="title"
+                label={t("form.section.personalDetails.questions.title")}
+                required={true}
+              />
+              <FormTextField
+                IdAndName="forenames"
+                label={t("form.section.personalDetails.questions.forenames")}
+                required={true}
+              />
+              <FormTextField
+                IdAndName="surname"
+                label={t("form.section.personalDetails.questions.surname")}
+                required={true}
+              />
+              <FormTextField
+                IdAndName="address"
+                label={t("form.section.personalDetails.questions.address")}
+                required={true}
+              />
+              <FormTextField
+                IdAndName="postCode"
+                label={t("form.section.personalDetails.questions.postCode")}
+                required={true}
+              />
+              <FormTextField
+                IdAndName="mobile"
+                label={t("form.section.personalDetails.questions.mobile")}
+                required={true}
+              />
+              <FormTextField
+                IdAndName="home"
+                label={t("form.section.personalDetails.questions.home")}
+                required={false}
+              />
+              <FormTextField
+                IdAndName="work"
+                label={t("form.section.personalDetails.questions.work")}
+                required={false}
+              />
+              <FormTextField
+                IdAndName="email"
+                label={t("form.section.personalDetails.questions.email")}
+                required={true}
+              />
+            </FormFieldsGroup>
+          </FormSection>
+          <FormSection>
+            <FormSideBar
+              title={t("form.section.employment.title")}
+              paragraph={t("form.section.employment.description")}
+            />
+            <FormFieldsGroup></FormFieldsGroup>
+          </FormSection>
+          <FormSection>
+            <FormSideBar
+              title={t("form.section.education.title")}
+              paragraph={t("form.section.education.description")}
+            />
+            <FormFieldsGroup></FormFieldsGroup>
+          </FormSection>
+          <FormSection>
+            <FormSideBar
+              title={t("form.section.practicalSkills.title")}
+              paragraph={t("form.section.practicalSkills.description")}
+            />
+            <FormFieldsGroup></FormFieldsGroup>
+          </FormSection>
+          <FormSection>
+            <FormSideBar
+              title={t("form.section.statement.title")}
+              paragraph={t("form.section.statement.description")}
+            />
+            <FormFieldsGroup></FormFieldsGroup>
+          </FormSection>
+          <FormSection>
+            <FormSideBar
+              title={t("form.section.interests.title")}
+              paragraph={t("form.section.interests.description")}
+            />
+            <FormFieldsGroup></FormFieldsGroup>
+          </FormSection>
+          <FormSection>
+            <FormSideBar
+              title={t("form.section.references.title")}
+              paragraph={t("form.section.references.description")}
+            />
+            <FormFieldsGroup></FormFieldsGroup>
+          </FormSection>
+          <FormSection>
+            <FormSideBar
+              title={t("form.section.other.title")}
+              paragraph={t("form.section.other.description")}
+            />
+            <FormFieldsGroup></FormFieldsGroup>
+          </FormSection>
+          <FormSection>
+            <FormSideBar
+              title={t("form.section.conflict.title")}
+              paragraph={t("form.section.conflict.description")}
+            />
+            <FormFieldsGroup></FormFieldsGroup>
+          </FormSection>
+          <FormSection>
+            <FormSideBar
+              title={t("form.section.convictions.title")}
+              paragraph={t("form.section.convictions.description")}
+            />
+            <FormFieldsGroup></FormFieldsGroup>
+          </FormSection>
+          <FormSection>
+            <FormSideBar
+              title={t("form.section.dataProtection.title")}
+              paragraph={t("form.section.dataProtection.description")}
+            />
+            <FormFieldsGroup></FormFieldsGroup>
+          </FormSection>
+          <FormSection>
+            <FormSideBar
+              title={t("form.section.declaration.title")}
+              paragraph={t("form.section.declaration.description")}
+            />
+            <FormFieldsGroup></FormFieldsGroup>
+          </FormSection>
+          <div className="md:col-span-5 text-right">
+            <div className="inline-flex items-end">
+              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                Submit
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </main>
+  );
+}
